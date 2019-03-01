64d9468e6df9dc3ce22b4f3eb9b96a0bcb9b546f
Peter Wunder <hallo@peterwunder.de>
2019-03-01 03:30:50 +0100

    Move from Gist to full repository
    Moved ffmpeg execution routine to own function
    There's less log spam now, thanks to -loglevel warning
    Reordered argparse arguments
    -vh now optionally takes a scaling factor as well
    -r now uses the fps filter internally
    Added --x264, --x265, --gif, --gif-palette, --apng, and --webp parameters
    Removed --apple argument since the videotoolbox encoder is garbage
    Video height is now ceil()ed to the next even number
    Made -ss an output option and added FAST_SEEK switch since fast seeking is imprecise

--- ffauto.py
+++ ffauto.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import subprocess
 import argparse
 import time
 import json
+import math
 from datetime import datetime as dt
+from tempfile import mkstemp
 
 def closest(num, arr):
 	filtered = filter(lambda x: x >= num, arr)
 	return min(filtered)
+
+def ceil_even(num):
+	return math.ceil(num / 2.0) * 2
 
 def parse_ffmpeg_timestamp(timestamp, debug):
 	timestamp_formats = ["%H:%M:%S.%f", "%H:%M:%S", "%M:%S.%f", "%M:%S"]
@@ -66,42 +72,38 @@ def get_video_info(video, debug):
 
 	return stream
 
-def main():
+def start_ffmpeg(args, debug):
+	if debug:
+		print("#" * 40)
+		print("Script arguments:")
+		print(" ".join(args))
+		print("#" * 40)
+		input("Press Enter to continue...")
+
 	start = time.time()
 
-	parser = argparse.ArgumentParser(description="Automate ffmpeg stuff")
-	parser.add_argument("-i", metavar="input", type=str, required=True, help="Input file")
-	parser.add_argument("-ss", metavar="start time", type=str, default="0", help="Start time")
+	p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
-	duration_group = parser.add_mutually_exclusive_group()
-	duration_group.add_argument("-t", metavar="duration", type=str, default=None, help="Duration")
-	duration_group.add_argument("-to", metavar="position", type=str, default=None, help="Position")
+	oldline = ""
+	while True:
+		line = p.stdout.readline().strip()
+		if line == "" and p.poll() is not None:
+			returncode = p.returncode
+			if returncode == 0:
+				# ffmpeg has successfully exited
+				end = time.time()
+				print(f"ffmpeg completed in {time.strftime('%H:%M:%S', time.gmtime(end - start))}")
+			else:
+				print(f"ffmpeg has terminated with code {p.returncode}")
+			break
 
-	audio_group = parser.add_mutually_exclusive_group()
-	audio_group.add_argument("-m", "--mute", action="store_true", help="Mute audio")
-	audio_group.add_argument("-af", "--audio-force", action="store_true", help="Force convert audio")
-	audio_group.add_argument("-av", "--volume", metavar="audio volume", type=str, default=None, help="Audio volume adjustment factor")
+		if line != oldline:
+			print(line)
 
-	parser.add_argument("-vt", "--title", metavar="video title", type=str, default=None, help="Video title")
-	parser.add_argument("-f", "--fade", metavar="fade duration", type=str, default=None, help="Fade in/out duration in seconds. Takes priority over -fi and -fo")
-	parser.add_argument("-fi", "--fadein", metavar="fade in duration", type=str, default=None, help="Fade in duration in seconds")
-	parser.add_argument("-fo", "--fadeout", metavar="fade out duration", type=str, default=None, help="Fade out duration in seconds")
-	parser.add_argument("-c", "--crop", metavar="w:h:x:y", type=str, default=None, help="New video region")
-	parser.add_argument("-r", "--framerate", metavar="target framerate", type=str, default=None, help="New video frame rate")
-	parser.add_argument("-vh", "--height", metavar="video height", type=str, default=None, help="New video height (keeps aspect ratio)")
-	parser.add_argument("-vc", "--codec", type=str, choices=["libx264", "libx265"], default="libx264", help="Codec choice")
-	parser.add_argument("-ff", "--ffmpeg", type=str, default=None, help="passthrough arguments for ffmpeg")
-	parser.add_argument("--fixrgb", type=str, default="0", help="Convert TV RGB range to PC RGB range (hacky)")
-	parser.add_argument("--debug", action="store_true", help="Debug mode (displays lots of additional information)")
+		oldline = line
 
-	extra_group = parser.add_mutually_exclusive_group()
-	extra_group.add_argument("-yt", "--youtube", action="store_true", help="YouTube mode (adds options to make YouTube happy)")
-	extra_group.add_argument("-nv", "--nvidia", action="store_true", help="Enable hardware acceleration for Nvidia GPUs (experimental)")
-	extra_group.add_argument("-ap", "--apple", action="store_true", help="Enable hardware acceleration for macOS (experimental)")
-
-	parser.add_argument("out", type=str, help="out file")
-
-	args = parser.parse_args()
+def main():
+	FAST_SEEK = False
 
 	CRF_X264 = 20
 	CRF_X265 = 24
@@ -130,14 +132,44 @@ def main():
 		}
 	}
 
-	if args.fade:
-		args.fadein = args.fade
-		args.fadeout = args.fade
+	parser = argparse.ArgumentParser(description="Automate ffmpeg stuff")
+	parser.add_argument("-i", metavar="input", type=str, required=True, help="Input file")
+	parser.add_argument("-ss", metavar="start time", type=str, default="0", help="Start time")
 
-	if args.nvidia:
-		args.codec = "h264_cuvid"
-	elif args.apple:
-		args.codec = "h264_videotoolbox"
+	duration_group = parser.add_mutually_exclusive_group()
+	duration_group.add_argument("-t", metavar="duration", type=str, default=None, help="Duration")
+	duration_group.add_argument("-to", metavar="position", type=str, default=None, help="Position")
+
+	audio_group = parser.add_mutually_exclusive_group()
+	audio_group.add_argument("-m", "--mute", action="store_true", help="Mute audio")
+	audio_group.add_argument("-af", "--audio-force", action="store_true", help="Force convert audio")
+	audio_group.add_argument("-av", "--volume", metavar="audio volume", type=str, default=None, help="Audio volume adjustment factor")
+
+	parser.add_argument("-vt", "--title", metavar="video title", type=str, default=None, help="Video title")
+	parser.add_argument("-f", "--fade", metavar="fade duration", type=str, default=None, help="Fade in/out duration in seconds. Takes priority over -fi and -fo")
+	parser.add_argument("-fi", "--fadein", metavar="fade in duration", type=str, default=None, help="Fade in duration in seconds")
+	parser.add_argument("-fo", "--fadeout", metavar="fade out duration", type=str, default=None, help="Fade out duration in seconds")
+	parser.add_argument("-c", "--crop", metavar="w:h:x:y", type=str, default=None, help="New video region")
+	parser.add_argument("-r", "--framerate", metavar="target framerate", type=str, default=None, help="New video frame rate")
+	parser.add_argument("-vh", "--height", metavar="video height", type=str, default=None, help="New video height (keeps aspect ratio)")
+	parser.add_argument("-ff", "--ffmpeg", type=str, default=None, help="Passthrough arguments for ffmpeg")
+	parser.add_argument("-gp", "--gif-palette", type=str, default="256", help="Number of colors to use when generating a GIF palette")
+	parser.add_argument("--fixrgb", type=str, default="0", help="Convert TV RGB range to PC RGB range (hacky)")
+	parser.add_argument("--debug", action="store_true", help="Debug mode (displays lots of additional information)")
+
+	extra_group = parser.add_mutually_exclusive_group()
+	extra_group.add_argument("-yt", "--youtube", action="store_true", help="YouTube mode (adds options to make YouTube happy)")
+	extra_group.add_argument("-nv", "--nvidia",  action="store_true", help="Enable hardware acceleration for Nvidia GPUs (experimental)")
+
+	extra_group.add_argument("--x264", action="store_true", help="Use libx264")
+	extra_group.add_argument("--x265", action="store_true", help="Use libx265")
+	extra_group.add_argument("--gif",  action="store_true", help="Create an animated GIF")
+	extra_group.add_argument("--apng", action="store_true", help="Create an animated PNG")
+	extra_group.add_argument("--webp", action="store_true", help="Create an animated WebP image (untested)")
+
+	parser.add_argument("out", type=str, help="out file")
+
+	args = parser.parse_args()
 
 	video_info = get_video_info(args.i, args.debug)
 
@@ -149,7 +181,35 @@ def main():
 	else:
 		duration_secs = video_info["duration"] - start_secs
 
-	fadeout_start = round(duration_secs - float(args.fadeout or 0), 4)
+	if args.youtube or args.x264:
+		args.codec = "libx264"
+	elif args.x265:
+		args.codec = "libx265"
+	elif args.nvidia:
+		args.codec = "h264_cuvid"
+	elif args.gif:
+		args.codec = "gif"
+	elif args.apng:
+		args.codec = "apng"
+	elif args.webp:
+		args.codec = "libwebp"
+	else:
+		args.codec = "libx264" # default codec
+
+	if args.gif:
+		# for GIF creation, fast seek needs to be enabled
+		FAST_SEEK = True
+
+	if args.fade:
+		args.fadein = args.fade
+		args.fadeout = args.fade
+
+	if FAST_SEEK:
+		fadein_start  = 0
+		fadeout_start = round(duration_secs - float(args.fadeout or 0), 4)
+	else:
+		fadein_start  = round(start_secs, 4)
+		fadeout_start = round(start_secs + duration_secs - float(args.fadeout or 0), 4)
 
 	if args.crop:
 		crop_params = args.crop.split(":")
@@ -157,10 +217,17 @@ def main():
 			crop_width, crop_height, crop_x, crop_y = crop_params
 
 	if args.height:
-		if args.nvidia:
-			filter_scale = f"scale_cuda=-1:{int(args.height)}"
+		if args.height.lower().endswith("x") or args.height.lower().endswith("×"):
+			new_video_height = int(float(video_info["height"]) * float(args.height[:-1]))
 		else:
-			filter_scale = f"scale=-1:{int(args.height)}:flags=spline+accurate_rnd+full_chroma_int+full_chroma_inp"
+			new_video_height = int(args.height)
+
+		new_video_height = ceil_even(new_video_height)
+
+		if args.nvidia:
+			filter_scale = f"scale_cuda=-2:{new_video_height}"
+		else:
+			filter_scale = f"scale=-2:{new_video_height}:flags=spline+accurate_rnd+full_chroma_int+full_chroma_inp"
 	else:
 		filter_scale = None
 
@@ -170,8 +237,11 @@ def main():
 	# Force the video output to full range RGB as well (if fixrgb == 2)
 	filter_fixrgb = "scale=in_range=tv:out_range=pc" if int(args.fixrgb) == 2 else None
 
-	filter_vfadein = f"fade=t=in:st=0:d={args.fadein}" if args.fadein else None
+	filter_vfadein = f"fade=t=in:st={fadein_start}:d={args.fadein}" if args.fadein else None
 	filter_vfadeout = f"fade=t=out:st={fadeout_start}:d={args.fadeout}" if args.fadeout else None
+
+	filter_palettegen = f"palettegen=stats_mode=diff:reserve_transparent=0:max_colors={args.gif_palette}" if args.gif else None
+	filter_paletteuse = f"paletteuse=diff_mode=rectangle:dither=floyd_steinberg"
 
 	if args.nvidia:
 		if args.fadein and args.fadeout:
@@ -183,19 +253,23 @@ def main():
 			filter_vfadeout = f"hwdownload,format=nv12,{filter_vfadeout},hwupload" if args.fadeout else None
 
 	filter_avolume = f"volume={args.volume}" if args.volume else None
-	filter_afadein = f"afade=t=in:st=0:d={args.fadein}:curve=ihsin" if args.fadein else None
+	filter_afadein = f"afade=t=in:st={fadein_start}:d={args.fadein}:curve=ihsin" if args.fadein else None
 	filter_afadeout = f"afade=t=out:st={fadeout_start}:d={args.fadeout}:curve=ihsin" if args.fadeout else None
 
+	filter_fps = f"fps=fps={args.framerate}" if args.framerate else None
+
 	filter_crop = f"crop={crop_width}:{crop_height}:{crop_x}:{crop_y}" if args.crop else None
+
+	filter_sharpen = "unsharp" if args.gif else None
 
 	filter_vfade = ",".join(filter(None, [filter_vfadein, filter_vfadeout]))
 	filter_audio = ",".join(filter(None, [filter_avolume, filter_afadein, filter_afadeout]))
 
+	opt_passthrough = args.ffmpeg.split(" ") if args.ffmpeg else []
+
 	opt_metadata = ["-metadata", f"title=\"{args.title}\""] if args.title else []
 
-	opt_framerate = ["-r", args.framerate] if args.framerate else []
-
-	opt_vcodec = args.codec
+	opt_global = "-loglevel warning -hide_banner".split(" ")
 
 	convert_audio = args.audio_force or args.volume or (args.fadein or args.fadeout)
 
@@ -205,80 +279,81 @@ def main():
 
 	opt_duration = ["-t", f"{duration_secs:.4f}"] if args.t or args.to else []
 
-	opt_vfilter_joined = ",".join(filter(None, [filter_crop, filter_scale, filter_fixrgb, filter_vfade]))
+	opt_vfilter_joined = ",".join(filter(None, [filter_fps, filter_fixrgb, filter_crop, filter_scale, filter_sharpen, filter_vfade, filter_palettegen]))
 	opt_vfilter = ["-vf", opt_vfilter_joined] if opt_vfilter_joined else []
 
 	yt_index1 = closest(video_info["r_frame_rate"], YT_BITRATES.keys())
 	yt_index2 = closest(video_info["height"], YT_BITRATES[yt_index1].keys())
 	yt_bitrate = YT_BITRATES[yt_index1][yt_index2]
-	opt_youtube = ["-movflags", "faststart", 
+	opt_youtube = ["-movflags", "faststart",
 				   "-maxrate", yt_bitrate,
 				   "-bufsize", f"{round(int(yt_bitrate[:-1])*1.5)}M",
 				   "-g", f"{video_info['r_frame_rate'] / 2}",
 				   "-bf", "2",
 				   "-pix_fmt", "yuv420p"] \
-				   if args.youtube and not args.hardware else []
+				   if args.youtube else []
 
 	opt_nv_hwaccel = "-hwaccel cuvid".split(" ") if args.nvidia else []
-	opt_ap_hwaccel = "-hwaccel videotoolbox".split(" ") if args.apple else []
+	opt_hardware = opt_nv_hwaccel
 
 	CODEC_OPTIONS = {
-		"libx264": f"-crf {CRF_X264} -preset {PRESET} -tune film -profile high -level 5.2".split(" ") + opt_fixrgb + opt_youtube,
-		"libx265": f"-crf {CRF_X265} -preset {PRESET} -tune film -profile high -level 5.2".split(" "),
-		"h264_cuvid": f"-preset {PRESET} -profile high -level 5.2 -rc constqp -qp {QP_NVENC} -strict_gop true -rc-lookahead 48 -spatial-aq true -temporal-aq true -aq-strength 8".split(" "),
-		"h264_videotoolbox": f"-profile high -level 5.1 -coder cabac".split(" ")
+		"libx264": f"-crf {CRF_X264} -preset {PRESET} -tune film -profile:v high -level 5.2".split(" ") + opt_fixrgb + opt_youtube,
+		"libx265": f"-crf {CRF_X265} -preset {PRESET} -tune film -profile:v high -level 5.2".split(" "),
+		"h264_cuvid": f"-preset {PRESET} -profile:v high -level 5.2 -rc constqp -qp {QP_NVENC} -strict_gop true -rc-lookahead 48 -spatial-aq true -temporal-aq true -aq-strength 8".split(" "),
+		"gif": f"-f gif -loop 0".split(" "),
+		"apng": f"-f apng -plays 0".split(" "),
+		"libwebp": f"-f webp -loop 0".split(" ")
 	}
 
-	ffmpeg_args = ["ffmpeg"] + \
-						opt_nv_hwaccel + opt_ap_hwaccel + \
-					   ["-ss", args.ss,
-						"-i", f"{args.i}",
-						"-c:v", args.codec] + \
+	if FAST_SEEK:
+		opt_input = ["-ss", args.ss, "-i", args.i]
+	else:
+		opt_input = ["-i", args.i, "-ss", args.ss]
+
+	opt_input += opt_duration
+	opt_codec = CODEC_OPTIONS[args.codec]
+
+	if args.gif:
+		# exporting a GIF
+		_, palette_file = mkstemp(prefix="palette_", suffix=".png")
+		ffmpeg_args = ["ffmpeg"] + opt_global + \
+						opt_input + \
+						["-c:v", args.codec] + opt_codec + \
 						opt_audio + opt_afilter + \
-						CODEC_OPTIONS[args.codec] + \
-						opt_framerate + \
-						opt_vfilter + opt_metadata + opt_duration + \
-						(args.ffmpeg.split(" ") if args.ffmpeg else []) + \
-						["-y", f"{args.out}"]
+						opt_vfilter + opt_metadata + opt_passthrough + \
+						["-y", palette_file]
+		print("Creating GIF palette…")
+	else:
+		# exporting a video, an APNG, or an animated WebP image
+		ffmpeg_args = ["ffmpeg"] + opt_global + \
+						opt_hardware + opt_input + \
+						["-c:v", args.codec] + opt_codec + \
+						opt_audio + opt_afilter + \
+						opt_vfilter + opt_metadata + opt_passthrough + \
+						["-y", args.out]
+		print("Encoding output file…")
 
-	if args.debug:
-		print("#" * 40)
+	start_ffmpeg(ffmpeg_args, args.debug) # first pass
 
-		print("Script arguments:\n", args)
+	if args.gif:
+		print("Creating GIF…")
+		# do second GIF creation pass
+		opt_input += ["-i", palette_file]
 
-		print("ffprobe output:", video_info)
-		print("Start time in seconds:", f"{start_secs:.4f}")
-		print("Duration in seconds:", f"{duration_secs:.4f}")
-		print("Video fade filters:", filter_vfade)
-		print("Audio filters:", filter_audio)
+		opt_vfilter_joined = ",".join(filter(None, [filter_fps, filter_fixrgb, filter_crop, filter_scale, filter_vfade, filter_paletteuse]))
+		opt_vfilter = ["-lavfi", opt_vfilter_joined] if opt_vfilter_joined else []
 
-		if args.youtube:
-			print("YouTube arguments:\n", " ".join(opt_youtube))
+		ffmpeg_args = ["ffmpeg"] + opt_global + \
+						opt_input + \
+						["-c:v", args.codec] + opt_codec + \
+						opt_audio + opt_afilter + \
+						opt_vfilter + opt_metadata + opt_passthrough + \
+						["-y", args.out]
 
-		print("ffmpeg arguments:\n", " ".join(ffmpeg_args))
+		start_ffmpeg(ffmpeg_args, args.debug)
 
-		print("#" * 40)
-		input("Press Enter to continue...")
-
-	p = subprocess.Popen(ffmpeg_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-
-	oldline = ""
-	while True:
-		line = p.stdout.readline().strip()
-		if line == "" and p.poll() is not None:
-			returncode = p.returncode
-			if returncode == 0:
-				# ffmpeg has successfully exited
-				end = time.time()
-				print(f"ffmpeg completed in {time.strftime('%H:%M:%S', time.gmtime(end - start))}")
-			else:
-				print(f"ffmpeg has terminated with code {p.returncode}")
-			break
-
-		if line != oldline:
-			print(line)
-
-		oldline = line
+		# clean up
+		os.remove(palette_file)
 
 if __name__ == '__main__':
 	main()
--- readme.md
+++ readme.md
@@ -1,34 +1,64 @@
 # ffauto.py
 
 ## What's this?
-This is a Python 3 script that aims to make some things that are complicated to do with ffmpeg easier.
+This is a Python 3 script that aims to be a "swiss army knife" script for some common ffmpeg tasks.
 
 ## What is this not?
 This isn't a script to make video encoding particularly efficient or video files particularly small. In fact, this script focuses more on video quality than on video size. If you're out to find the perfect size/quality ratio, this is not what you're looking for.
 
 Also, unless you explicitly enable it, the encoding method that's used here won't use hardware acceleration. This means that if you process a long video file with this script, you most likely won't be able to use the computer for anything else for *a while*.
-    
-## All options, explained:
+
+## All of `ffauto`'s options
+
+### Required options
+
 * `-i`: The input video file.
-* `-ss`: The position in the input video to skip to before doing anything. Can be either a timestamp (supported formats are below) or a float (for seconds). Default is 0, starting at the beginning.
-  * The supported timestamp formats are "%H:%M:%S.%f", "%H:%M:%S", "%M:%S.%f", and "%M:%S"
+* `out`: The output file. Must be the last parameter.
+
+### General options
+
+* `-ss`: The position in the input video to skip to before doing anything. Can be either a timestamp (supported formats are below) or a float (for seconds).
+	* The supported timestamp formats are `%H:%M:%S.%f`, `%H:%M:%S`, `%M:%S.%f`, `%M:%S`, `%S.%f`, and `%S`.
 * `-t`: The duration of the output video. Supports the same arguments as `-ss`. Can't be used with `-to`.
-  * `-to`: Stops the output video at this position. Supports the same arguments as `-ss`. Can't be used with `-t`.
+* `-to`: Stops the output video at this position. Supports the same arguments as `-ss`. Can't be used with `-t`.
 * `-vt/--title`: The video title to be embedded in the output video's metadata. Must be enclosed in double quotes.
-* `-m/--mute`: Mutes the output video. Takes no arguments.
-* `-f/--fade`: Applies both a fade-in and a fade-out to the output video. The duration in seconds is expected as an integer or a float.
-  * `-fi/--fadein`: Applies a fade-in to the output video. Takes the same arguments as `-f`. Is ignored if `-f` is present.
-  * `-fo/--fadeout`: Applies a fade-out to the output video. Takes the same arguments as `-f`. Is ignored if `-f` is present.
+* `-gp/--gif-palette`: The number of colors to use when creating an animated GIF. Ignored if `--gif` isn't specified.
+* `-ff/--ffmpeg`: Passthrough arguments for ffmpeg.
+
+### Video options
+#### All options listed in this category are applied in the order of appearance.
+
+* `-r/--framerate`: Changes the output video's framerate. Expects a positive number.
+* `--fixrgb`: A leftover command, but it might be useful for some. It accepts the integer values 1 or 2, depending on which it does this:
+	* `1`: Nothing is converted, only the color metadata is inserted. Useful in cases where the source material is implied to contain full range video but isn't read as such by other programs.
+	* `2`: `ffauto.py` will assume that the source video has a limited RGB range and will force-convert it to full RGB range. Then, it will embed all necessary color metadata just to be safe.
 * `-c/--crop`: Crops the video. Uses the same syntax as ffmpeg: `width:height:x:y`
-* `-vh/--height`: Resizes the video to be this many pixels high using Lanczos interpolation, preserving the aspect ratio. Expects a positive integer argument.
-  * The lack of an option to resize to a certain width is intentional.
-* `-vc/--codec`: Accepts either "libx264" or "libx265". Default is "libx264".
-* `-yt/--youtube`: Applies a bunch of options to make the video as YouTube-friendly as possible.  No more "This video needs to be in a streamable format" warnings. Only works with libx264 and can't be used with hardware acceleration.
-* `-nv/--nvidia`: Enables hardware acceleration for compatible Nvidia GPUs. Requires an ffmpeg build with support for the CUDA SDK, NVENC, and CUVID. Can't be used with `-yt` or `-ap`.
-* `-ap/--apple`: Enables hardware acceleration for macOS. Unless your CPU is exceptionally slow, conventional software-accelerated encoding will both be faster and better-looking. Can't be used with `-yt` or `-nv`.
-* `--fixrgb`: Basically a leftover command, but it might be useful for some. It accepts the integer values 1 or 2, depending on which it does this:
-  * `1`: Nothing is converted, only the color metadata is inserted. Useful in cases where the source material is implied to contain full range video but isn't read as such by other programs.
-  * `2`: `ffauto.py` will assume that the source video has a limited RGB range and will force-convert it to full RGB range. Then, it will embed all necessary color metadata just to be safe.
+* `-vh/--height`: Resizes the video to be this many pixels high using Lanczos interpolation, preserving the aspect ratio. Expects a positive number. Final video height is rounded to the next even number.
+	* To use scaling factors instead of absolute pixel heights, append an "x" to the argument. For example, `-vh 0.5x` will halve a video's height.
+* `-f/--fade`: Applies both a fade-in and a fade-out to the output video. The duration in seconds is expected as a number.
+	* `-fi/--fadein`: Applies a fade-in to the output video. Takes the same arguments as `-f`. Is ignored if `-f` is present.
+	* `-fo/--fadeout`: Applies a fade-out to the output video. Takes the same arguments as `-f`. Is ignored if `-f` is present.
+
+### Audio options
+
+* `-m/--mute`: Mutes the output video. Takes no arguments.
+* `-av/--volume`: Changes the audio volume by a specified factor. Expects a positive number.
+* `-af/--audio-force`: Forces audio reencoding.
+
+### Format Options, mutually exclusive
+
+* `-yt/--youtube`: Applies a bunch of options to make the video as YouTube-friendly as possible. No more "This video needs to be in a streamable format" warnings. Will set the codec to H.264.
+* `-nv/--nvidia`: Enables hardware acceleration for compatible Nvidia GPUs. Requires an ffmpeg build with support for the CUDA SDK, NVENC, and CUVID.
+* `--x264`: Tells ffmpeg to use the `libx264` encoder. **(Default)**
+* `--x265`: Tells ffmpeg to use the `libx265` encoder.
+* `--gif`: Creates an animated GIF from the input video.
+* `--apng`: Creates an animated PNG from the input video
+* `--webp`: Creates an animated WebP image from the input video. **(Untested)**
+
+
+### Debugging commands
+
+
 * `--debug`: Prints some additional debugging info and requires a keypress before actually starting to process video files.
 
 ## About hardware acceleration:
@@ -41,12 +71,12 @@ Don't be surprised if video files you create using hardware acceleration are thr
 
 ## Usage examples:
 ### Skip the first x seconds of a video
-    ./ffauto.py -i VIDEO_FILE -ss x OUT_FILE
+	./ffauto.py -i VIDEO_FILE -ss x OUT_FILE
 ### Only take the first x seconds of a video
-    ./ffauto.py -i VIDEO_FILE -t x OUT_FILE
+	./ffauto.py -i VIDEO_FILE -t x OUT_FILE
 ### Start at 2:35 minutes and take the next 35.5 seconds
-    ./ffauto.py -i VIDEO_FILE -ss 2:35 -t 35.5 OUT_FILE
+	./ffauto.py -i VIDEO_FILE -ss 2:35 -t 35.5 OUT_FILE
 ### Resize a video to be 720 pixels high, add a fade-out effect lasting 0.5 seconds, limit the video length to 30 seconds, and embed a video title
-    ./ffauto.py -i VIDEO_FILE -vh 720 -fo 0.5 -t 30 -vt "title goes here" OUT_FILE
+	./ffauto.py -i VIDEO_FILE -vh 720 -fo 0.5 -t 30 -vt "title goes here" OUT_FILE
 ### Add both a fade-in and a fade-out effect lasting half a second, limit the video to 50 seconds and prepare it for a YouTube upload
-    ./ffauto.py -i VIDEO_FILE -f 0.5 -t 50 -yt OUT_FILE
+	./ffauto.py -i VIDEO_FILE -f 0.5 -t 50 -yt OUT_FILE
